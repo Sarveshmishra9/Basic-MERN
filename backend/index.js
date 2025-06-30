04e955ce15325a327b86703499ac7fecd1daff47
Sarveshmishra9 <sarvesh.22b0151026@abes.ac.in>
2025-06-30 12:43:31 +0530

    mern

--- backend/index.js
+++ backend/index.js
@@ -19,26 +19,4 @@ mongoose.connect(process.env.DBURL).then(() => {
     })
 }).catch((err) => { console.log(err) });
 
-// let express=require("express")
-// let mongoose=require("mongoose");
-// let cors = require('cors');
-// const enquiryRouter = require("./App/routes/web/enquiryRoutes");
-// require('dotenv').config();
-// let app = express();
-// app.use(cors())
-
-// app.use(express.json());
-
-// //Routes
-// app.use('/api/website/enquiry',enquiryRouter); 
-
-
-
-// //connect to mongodb
-// mongoose.connect(process.env.DBURL).then(()=>{
-//     console.log("connected to mongodb");
-//     app.listen(process.env.PORT || 3000,()=>{
-//         console.log("server is running");
-//     })
-// }).catch((err)=>{console.log(err)});
 
