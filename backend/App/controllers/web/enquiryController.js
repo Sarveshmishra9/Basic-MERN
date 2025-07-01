5a9b675df8596a25fd2d44d56373047cd1e5d153
Sarveshmishra9 <sarvesh.22b0151026@abes.ac.in>
2025-07-02 01:08:49 +0530

    again

--- backend/App/controllers/web/enquiryController.js
+++ backend/App/controllers/web/enquiryController.js
@@ -33,35 +33,3 @@ let enquiryUpdate = async (req, res) => {
 module.exports = { enquiryInsert, enquiryList, enquiryDelete, enquiryUpdate };
 
 
-
-
-// //enquiryController.js
-// const enquiryModel = require("../../models/enquiry.model");
-
-// let enquiryInsert=(req,res)=>{
-//     let {name,email,phone,message}=req.body;
-//     let enquiry=new enquiryModel({
-//     name,
-//     email,
-//     phone,
-//     message
-//     });
-//     enquiry.save().then(()=>{
-//         res.send({status:1,message:"enquiry saved"});
-
-//     }).catch((err)=>{
-//         res.send({status:0,message:"error while save enquiry",error:err});
-//     })
-// }
-
-// let enquiryList=async(req,res)=>{
-//     let enquiry=await enquiryModel.find();
-//     res.send({status:1,enquiryList:enquiry});
-// }
-
-// let enquiryDelete=async(req,res)=>{
-//     let enId=req.params.id;
-//     let enquiry=await enquiryModel.find();
-//     res.send({status:1,message:"Enquiry delete succesfully",enquiry});
-// }
-// module.exports={enquiryInsert,enquiryList,enquiryDelete};
