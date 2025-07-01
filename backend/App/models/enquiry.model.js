8b41e401858c6cd4ffb54709cd3c764bb08456c9
Sarveshmishra9 <sarvesh.22b0151026@abes.ac.in>
2025-07-02 01:06:43 +0530

    mern

--- backend/App/models/enquiry.model.js
+++ backend/App/models/enquiry.model.js
@@ -10,27 +10,3 @@ const enquirySchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model("enquiry", enquirySchema);
-// let mongoose=require('mongoose');
-// let Schema=mongoose.Schema;
-// let enquirySchema=new Schema({ //schema->table
-//     name:{
-//         type:String,
-//         required:true
-//     },
-//     email:{
-//         type:String,
-//         required:true,
-//         unique:true
-//     },
-//     phone:{
-//         type:String,
-//         required:true
-//     },
-//     message:{
-//         type:String,
-//         required:true
-//     }
-// });
-
-// let enquiryModel=mongoose.model("enquiry",enquirySchema)//"enquiry" ->collection ,userEnquirySchema->field
-// module.exports=enquiryModel;
