b375908f0c51b75dc94668073fed44e062be6812
Sarveshmishra9 <sarvesh.22b0151026@abes.ac.in>
2025-07-03 23:13:35 +0530

    changes

--- backend/App/routes/web/enquiryRoutes.js
+++ backend/App/routes/web/enquiryRoutes.js
@@ -11,14 +11,3 @@ enquiryRouter.put("/update/:id", enquiryUpdate)
 module.exports = enquiryRouter;
 
 
-
-
-// //enquiryRoutes.js
-// let express=require('express');
-// const { enquiryInsert, enquiryList, enquiryDelete } = require('../../controllers/web/enquiryController');
-// let enquiryRouter=express.Router();
-
-// enquiryRouter.post("/insert",enquiryInsert)
-// enquiryRouter.post("/view",enquiryList)
-// enquiryRouter.delete("/delete/:id",enquiryDelete)
-// module.exports=enquiryRouter;
