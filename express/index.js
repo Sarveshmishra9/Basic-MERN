3d69c292350b46ccf917eb1b4ee59d4d4f589ac0
Sarveshmishra9 <sarvesh.22b0151026@abes.ac.in>
2025-07-13 03:39:55 +0530

    update

--- express/index.js
+++ express/index.js
@@ -2,8 +2,8 @@ let express=require("express")
 let app=express()
 //Handling JSON Data in ExpressJS
 app.use(express.json());
-
-let checkToken=(req,res,next)=>{//custom ye application middleWare
+//custom ye application middleWare
+let checkToken=(req,res,next)=>{
     console.log("Welcome")
     next();
 }
@@ -23,9 +23,9 @@ app.get("/news/:id",(req,res)=>{
     let CurrentId=req.params.id // acces  the param ki value 
     res.send("dynamic param"+CurrentId)
 })
-
+//object
 app.get("/login",(req,res)=>{
-    console.log(req.body)//object
+    console.log(req.body)
     res.send({status:1,msg:"login API",bodyData:req.body,queryData:req.query})
 })
 
